7a8e83af078b7f74572dd039dba6f7f9fcaca65e
Vera Lippgardt <vera1078@mail.ru>
2022-11-12 09:13:50 +0300

    задача с использованием reduce

--- seminar6.py
+++ seminar6.py
@@ -5,18 +5,26 @@
 #     Для n=4 {1: 2, 2: 2.25, 3: 2.37, 4: 2.44}
 #     Сумма 9.06
 
+from functools import reduce
 print('Задача на вывод списка из n чисел последовательности (1+1/n)^n и их суммы')
 n = int(input("Введите целое положительное число: "))
 numbers = {n : round(pow((1 + 1 / n), n), 2) for n in range(1, n + 1)}
 
 val_sum = 0
-for j in numbers.values():
-    val_sum += j
+# for j in numbers.values():
+#     val_sum += j
 
-# sum = lambda x : x for j in numbers.values() val_sum += x)
+# reduce из последовательности возвращает 1 значение (здесь -> сумму)
+sum1 = reduce(lambda val_sum, x: val_sum + x, numbers.values())             
+
 
 print(f'Для n = {n} {numbers}')
-print(f'Сумма значений = {round(val_sum, 2)}')
+print(f'Сумма значений = {sum1}')
 
-print()
-print()
+
+
+
+
+
+
+
