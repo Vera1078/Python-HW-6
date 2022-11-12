0657f620e6f5bf4e0294379c89ced379c9f4d3cf
Vera Lippgardt <vera1078@mail.ru>
2022-11-12 09:21:13 +0300

    задача с использованием map и filter

--- seminar6_1.py
+++ seminar6_1.py
@@ -1,4 +1,4 @@
-
+ 
 # Задача 3. Задайте последовательность чисел. Напишите программу, которая
 # выведет список неповторяющихся элементов исходной последовательности.
 # Пример:
@@ -9,12 +9,10 @@ print('Список неповторяющихся элементов исход
 print('Введите числа через пробел')
 list1 = list (map(int, input().split()))
 list2 = []
-# for i in list1:
-#     if list1.count (i) ==1:
-#         list2.append(i)
 
-list2 = filter(lambda )
+# for i in list1:
+#if list1.count (i) ==1:
+#  list2.append(i)
+
+list2 = list(filter(lambda x: list1.count (x) == 1, list1))
 print(list2)
-    
-print()
-print()    
